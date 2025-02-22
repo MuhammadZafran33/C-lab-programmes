8c7bb8172be7827034331b4b51f4521c579928cd
muhammad zafran IMSCIENCES <zafrankhaan33@gmail.com>
2025-02-22 11:01:15 +0500

    Add files via upload

---
+++ problem5.3.cpp
@@ -0,0 +1,33 @@
+// This program demonstrates a simple while loop.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int number = 0;
+
+    // while (number < 5)
+    // {
+    // cout << "Hello\n";
+    // number++;
+    // }
+    // cout << "That's all!\n";
+
+    // int number = 0;
+    // while (number < 5) // This semicolon is an ERROR!
+    // {
+    // cout << "Hello\n";
+    // number++;
+    // }
+
+// int remainder;int num;
+//     while (remainder == 1) // Error: Notice the assignment
+// {
+// cout << "Enter a number: ";
+// cin >> num;
+// remainder = num % 2;
+// }
+        
+    
+    return 0;
+}
