f9cdcdb1a229d1423a9ef6e2176413be49b85d32
muhammad zafran IMSCIENCES <zafrankhaan33@gmail.com>
2025-02-22 11:01:48 +0500

    Add files via upload

---
+++ TasksChap5.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int a,b,c;
+    // a = 2;
+    // b = 5;
+    // // c = a * b++;
+    // c = a * ++b;
+    // cout << a << " " << b << " " << c;
+    // return 0;
+
+    // int x, y;
+
+    // x = 10;
+    // // if (x++ > 10)
+    // if (++x > 10)
+    //     cout << "x is greater than 10.\n";
+
+    // x = 2;
+    // y = x++;
+    // cout <<"\n"<< x <<"\n"<< y;
+
+    // x = 2;
+    // y = ++x;
+    // cout <<"\n"<< x <<"\n"<< y;
+
+    // x = 2;
+    // y = 4;
+    // cout << x++ << --y;
+
+    // x = 2;
+    // y = 2 * x++;
+    // cout << x << y;
+
+    // x = 99;
+    // if (x++ < 100){
+    // cout<< "It is true!\n";
+    // }
+    // else
+    // {
+    //  cout << "It is false!\n";
+    // }
+
+    
+    int x, y;
+    x = 99;
+    if (++x>100)
+    {
+    cout<< "It is true!\n";
+    }
+    else
+    {
+     cout << "It is false!\n";
+    }
+    return 0;
+}
