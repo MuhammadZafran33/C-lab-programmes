df101cb1cc9763c79ddc5bbf59f0bfd88d7fd8bc
muhammad zafran IMSCIENCES <zafrankhaan33@gmail.com>
2025-03-13 11:29:41 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,12 +1,51 @@
 #include <iostream>
+
 using namespace std;
-void swap(int n1,int n2){
-	n1 = x;
-	n2 = y;
+
+int main() {
+//	int salary;
+//	int serviceYears;
+//	bool loan;
+//	cout<<"enter the salary of employee: "<<endl;
+//	cin>>salary;
+//	cout<<"enter the service years of employee: "<<endl;
+//	cin>>serviceYears;
+//	cout<<"have you availed loan? ";
+//	cin>>loan;
+//	if(loan==0){
+//	 if(salary<100000)
+//		if(serviceYears>5){
+//		
+//		cout<<"you are eligible for loan";
+//		
+//		cout<<"you are elegible ";
+//}
+//	}
+//	else
+//	cout<<"not elegible for loan ";
+//	
+
+
+    int k; 
+    int num; 
+    
+    
+    cout << "Enter the number for the multiplication table: ";
+    cin >> num;
+
+    cout << "Enter the range (k value): ";
+    cin >> k;
+    
+     
+    
+    for(int i = 1; i <= k; i++) {
+    	for(int j = 1; j <= k; j++) {
+    
+        cout << num << " x " << i << " = " << num * i << endl;
+        cout <<i<< j << ":\n";
+    }
+
 }
-int main(int argc, char** argv) {
-	int x=10;
-	int y =20;
-	swap(x, y);
-	return 0;
+    return 0;
 }
+
