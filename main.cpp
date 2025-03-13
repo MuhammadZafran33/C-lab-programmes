396d76602c582b3d9ec7839c41056dc97333ae92
muhammad zafran IMSCIENCES <zafrankhaan33@gmail.com>
2025-03-13 11:29:03 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,21 +1,12 @@
-// This program demonstrates the prefix and postfix
- // modes of the increment and decrement operators.
 #include <iostream>
 using namespace std;
-
-int main()
- {
- int num = 4;
-
- cout << num << endl; // Displays 4
- cout << num++ << endl; // Displays 4, then adds 1 to num
- cout << num << endl; // Displays 5
- cout << ++num << endl; // Adds 1 to num, then displays 6
- cout << endl; // Displays a blank line
- cout << num << endl; // Displays 6
- cout << num-- << endl; // Displays 6, then subtracts 1 from num
- cout << num << endl; // Displays 5
- cout << --num << endl; // Subtracts 1 from num, then displays 4
-
- return 0;
- }
+void swap(int n1,int n2){
+	n1 = x;
+	n2 = y;
+}
+int main(int argc, char** argv) {
+	int x=10;
+	int y =20;
+	swap(x, y);
+	return 0;
+}
